dd5d1a6954b1eb989d621882dfe69b4f61f15ab4
James Schwartz <james@schwartz.engineer>
2016-11-17 22:31:31 -0500

    Wrote distance function

--- src/Point.js
+++ src/Point.js
@@ -6,7 +6,10 @@ class Point {
 }
 
 function distance(p1, p2) {
-  
+  return Math.sqrt(
+    Math.pow((p1.x - p2.x), 2) +
+    Math.pow((p1.y - p2.y), 2)
+  );
 }
 
 export { Point, distance }
--- src/Point.test.js
+++ src/Point.test.js
@@ -5,5 +5,10 @@ import {Point, distance} from './Point';
 it('calculates distance correctly', () => {
   const p1 = new Point(0, 0);
   const p2 = new Point(2, 0);
+  const p3 = new Point(1, 1);
+
   expect(distance(p1, p2)).toBe(2);
+  expect(distance(p3, p1)).toBe(Math.sqrt(2));
+  expect(distance(p1, p3)).toBe(Math.sqrt(2));
+
 });
