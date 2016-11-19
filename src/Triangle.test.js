7fd64194d9dd06a01b340a88f860b21c766cb6fd
James Schwartz <james@schwartz.engineer>
2016-11-19 13:10:23 -0500

    Refactor triangle edge getter test for triangle holding indices

--- src/Triangle.test.js
+++ src/Triangle.test.js
@@ -16,15 +16,15 @@ it('calculates circumscribed circle correctly', () => {
 });
 
 it('returns a list of edges', () => {
-  const p1 = new Point(-1, 0);
-  const p2 = new Point(1, 0);
-  const p3 = new Point(0, 1);
+  const triangle = new Triangle(0, 1, 2);
+  const edges = triangle.edges;
 
-  const triangle = new Triangle(p1, p2, p3);
-  const result = triangle.edges;
+  expect(edges[0].includes(0)).toBeTruthy();
+  expect(edges[0].includes(1)).toBeTruthy();
 
-  // TODO: Does order matter?
-  expect(result.includes([p1, p2])).toBeTruthy();
-  expect(result.includes([p2, p3])).toBeTruthy();
-  expect(result.includes([p3, p1])).toBeTruthy();
+  expect(edges[1].includes(1)).toBeTruthy();
+  expect(edges[1].includes(2)).toBeTruthy();
+
+  expect(edges[2].includes(2)).toBeTruthy();
+  expect(edges[2].includes(0)).toBeTruthy();
 });
