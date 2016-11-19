28affcc4ef5329f4c571d6c239a255e3a9894d32
James Schwartz <james@schwartz.engineer>
2016-11-19 12:13:19 -0500

    Wrote triangle edges getter test case

--- src/Triangle.test.js
+++ src/Triangle.test.js
@@ -14,3 +14,17 @@ it('calculates circumscribed circle correctly', () => {
   expect(result.cy).toBe(0);
   expect(result.r).toBe(1);
 });
+
+it('returns a list of edges', () => {
+  const p1 = new Point(-1, 0);
+  const p2 = new Point(1, 0);
+  const p3 = new Point(0, 1);
+
+  const triangle = new Triangle(p1, p2, p3);
+  const result = triangle.edges;
+
+  // TODO: Does order matter?
+  expect(result.includes([p1, p2])).toBeTrue;
+  expect(result.includes([p2, p3])).toBeTrue;
+  expect(result.includes([p3, p1])).toBeTrue;
+});
