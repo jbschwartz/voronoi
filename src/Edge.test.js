7d69c050dc745c825d53bc7ac3f6d57b4308db3e
James Schwartz <james@schwartz.engineer>
2016-11-20 13:12:34 -0500

    Wrote Edge test.

---
+++ src/Edge.test.js
@@ -0,0 +1,17 @@
+import Edge from './Edge';
+import {Point} from './Point'
+
+describe('Edge', () => {
+  const start = new Point(0, 0);
+  const end = new Point(2, 0);
+
+  const edge = new Edge(start, end);
+
+  it('stores verticies in start and end', () => {
+    expect(edge.start.x).toEqual(start.x);
+    expect(edge.start.y).toEqual(start.y);
+
+    expect(edge.end.x).toEqual(end.x);
+    expect(edge.end.y).toEqual(end.y);
+  });
+})
