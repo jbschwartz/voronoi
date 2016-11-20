ed336ae959353f1a891d69b01a6de88e125cfc09
James Schwartz <james@schwartz.engineer>
2016-11-20 17:29:57 -0500

    Completely rewrite circumscribedCircle function to pass edge cases. Refactored for more clarity regarding algorithm.

--- src/Triangle.js
+++ src/Triangle.js
@@ -1,5 +1,6 @@
 import Circle from './Circle'
 import Edge from './Edge'
+import {Point, distance} from './Point'
 
 class Triangle {
   constructor(a, b, c) {
@@ -21,32 +22,87 @@ class Triangle {
   }
 }
 
+class Line {
+  constructor(point, slope) {
+    this.point = point;
+    this.m = slope;
+  }
+
+  isVertical() {
+    return !isFinite(this.m);
+  }
+
+  isHorizontal() {
+    return this.m === 0;
+  }
+
+  isParallel(line) {
+    return this.m === line.m;
+  }
+
+  intersect(line) {
+    if(this.isParallel(line)) return null;
+
+    let result = { x: null, y: null }
+
+    if(this.isHorizontal()) {
+      result.y = this.point.y;
+      result.x = line.evaluate({y: result.y});
+    } else {
+      if(this.isVertical()) {
+        result.x = this.point.x;
+        result.y = line.evaluate({x: result.x});
+      } else {
+        if(line.isVertical()) {
+          result.x = line.point.x;
+          result.y = this.evaluate({x: result.x});
+        } else {
+          result.x = (line.point.y - this.point.y + this.m * this.point.x - line.m * line.point.x) / (this.m - line.m);
+          result.y = line.evaluate({x: result.x});
+        }
+      }
+    }
+
+    return result;
+  }
+
+  evaluate(value) {
+    if(value.y !== undefined) {
+      if(this.isHorizontal()) return null;
+
+      return ((value.y - this.point.y) / this.m) + this.point.x
+    } else if(value.x !== undefined) {
+      if(this.isVertical()) return null;
+      return this.m * (value.x - this.point.x) + this.point.y;
+    }
+  }
+}
+
+function midpoint(a, b) {
+  return new Point(
+    (a.x + b.x) / 2,
+    (a.y + b.y) / 2
+  );
+}
+
+function slope(a, b) {
+  return (b.y - a.y) / (b.x - a.x);
+}
+
 function circumscribedCircle(points) {
-  const x1 = points[0].x;
-  const x2 = points[1].x;
-  const x3 = points[2].x;
+  const mA = slope(points[0], points[1]);
+  const mB = slope(points[1], points[2]);
 
-  const y1 = points[0].y;
-  const y2 = points[1].y;
-  const y3 = points[2].y;
+  const perpBisectorA = new Line(midpoint(points[0], points[1]), - 1 / mA);
+  const perpBisectorB = new Line(midpoint(points[1], points[2]), - 1 / mB);
 
-  var ma = (y2 - y1) / (x2 - x1);
-  var mb = (y3 - y2) / (x3 - x2);
+  const intersection = perpBisectorA.intersect(perpBisectorB);
 
-  if(ma == mb) {
+  if(intersection) {
+    return new Circle(intersection.x, intersection.y, distance(intersection, points[0]))
+  } else {
     return new Circle(NaN, NaN, NaN);
   }
-
-  var cx = (ma*mb*(y1-y3) + mb*(x1+x2) - ma*(x2+x3)) / (2 * (mb-ma));
-  if(ma != 0) {
-    var cy = -(1/ma)*(cx - (x1+x2)/2) + (y1+y2)/2
-  } else {
-    var cy = -(1/mb)*(cx - (x2+x3)/2) + (y2+y3)/2
-  }
-
-  var r = Math.sqrt(Math.pow((x1 - cx), 2) + Math.pow((y1 - cy), 2));
-
-  return new Circle(cx, cy, r);
 }
 
 export { Triangle }
