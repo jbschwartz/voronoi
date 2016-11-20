b54c7899c49f8d33e33d8b630cca512eda5ba55d
James Schwartz <james@schwartz.engineer>
2016-11-20 12:42:42 -0500

    Refactor Circle tests

--- src/Circle.test.js
+++ src/Circle.test.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Circle from './Circle';
 import {Point} from './Point';
 
-it('calculates contains correctly', () => {
+describe('Circle', () => {
   const p1 = new Point(-3, 0);
   const p2 = new Point(1, 0);
 
   const circle = new Circle(0, 0, 2);
 
-  expect(circle.contains(p1)).toBe(false);
-  expect(circle.contains(p2)).toBe(true);
-});
+  it('contains a point inside of it', () => {
+    expect(circle.contains(p2)).toBe(true);
+  });
+
+  it('does not contain a point outside of it', () => {
+    expect(circle.contains(p1)).toBe(false);
+  })
+})
