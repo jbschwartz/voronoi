745d14711c2dc412442a2026247b5074f1c081d0
James Schwartz <james@schwartz.engineer>
2016-11-20 14:22:46 -0500

    Write tests for degenerate triangle cases.

--- src/Triangle.test.js
+++ src/Triangle.test.js
@@ -18,6 +18,30 @@ describe('Triangle', () => {
       expect(circumCircle.r).toBe(1);
     });
 
+    it('handles degenerate vertical triangles', () => {
+      const b1 = new Point(1, 1);
+      const b2 = new Point(1, 2);
+      const b3 = new Point(1, 3);
+
+      const degenerate = new Triangle(b1, b2, b3);
+
+      expect(isNaN(degenerate.circumCircle.cx)).toBeTruthy();
+      expect(isNaN(degenerate.circumCircle.cy)).toBeTruthy();
+      expect(isNaN(degenerate.circumCircle.cx)).toBeTruthy();
+    });
+
+    it('handles degenerate horizontal triangles', () => {
+      const b1 = new Point(1, 1);
+      const b2 = new Point(2, 1);
+      const b3 = new Point(3, 1);
+
+      const degenerate = new Triangle(b1, b2, b3);
+
+      expect(isNaN(degenerate.circumCircle.cx)).toBeTruthy();
+      expect(isNaN(degenerate.circumCircle.cy)).toBeTruthy();
+      expect(isNaN(degenerate.circumCircle.cx)).toBeTruthy();
+    });
+
     it('is a Circle', () => {
       expect(circumCircle instanceof Circle).toBeTruthy();
     })
