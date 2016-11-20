aee9f85579d95f8de299d5c3f7022a85c6bc2715
James Schwartz <james@schwartz.engineer>
2016-11-20 13:33:15 -0500

    Refactor Triangle tests.

--- src/Triangle.test.js
+++ src/Triangle.test.js
@@ -1,28 +1,30 @@
 import {Point} from './Point';
 import {Triangle, circumscribedCircle} from './Triangle';
 
-it('calculates circumscribed circle correctly', () => {
-  const p1 = new Point(-1, 0);
-  const p2 = new Point(1, 0);
-  const p3 = new Point(0, 1);
+describe('Triangle', () => {
+  it('calculates circumscribed circle correctly', () => {
+    const p1 = new Point(-1, 0);
+    const p2 = new Point(1, 0);
+    const p3 = new Point(0, 1);
 
-  const result = circumscribedCircle([p1, p2, p3])
+    const result = circumscribedCircle([p1, p2, p3])
 
-  expect(result.cx).toBe(0);
-  expect(result.cy).toBe(0);
-  expect(result.r).toBe(1);
-});
+    expect(result.cx).toBe(0);
+    expect(result.cy).toBe(0);
+    expect(result.r).toBe(1);
+  });
 
-it('returns a list of edges', () => {
-  const triangle = new Triangle(0, 1, 2);
-  const edges = triangle.edges;
+  it('returns a list of edges', () => {
+    const triangle = new Triangle(0, 1, 2);
+    const edges = triangle.edges;
 
-  expect(edges[0].includes(0)).toBeTruthy();
-  expect(edges[0].includes(1)).toBeTruthy();
+    expect(edges[0].includes(0)).toBeTruthy();
+    expect(edges[0].includes(1)).toBeTruthy();
 
-  expect(edges[1].includes(1)).toBeTruthy();
-  expect(edges[1].includes(2)).toBeTruthy();
+    expect(edges[1].includes(1)).toBeTruthy();
+    expect(edges[1].includes(2)).toBeTruthy();
 
-  expect(edges[2].includes(2)).toBeTruthy();
-  expect(edges[2].includes(0)).toBeTruthy();
-});
+    expect(edges[2].includes(2)).toBeTruthy();
+    expect(edges[2].includes(0)).toBeTruthy();
+  });
+})
