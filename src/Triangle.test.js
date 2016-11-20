57ffb96780091c3e5aa1b15fdf10412918312556
James Schwartz <james@schwartz.engineer>
2016-11-20 13:47:35 -0500

    Clean up Triangle tests.

--- src/Triangle.test.js
+++ src/Triangle.test.js
@@ -1,6 +1,7 @@
 import {Point} from './Point';
 import Edge from './Edge';
-import {Triangle, circumscribedCircle} from './Triangle';
+import Circle from './Circle';
+import {Triangle} from './Triangle';
 
 describe('Triangle', () => {
   const p1 = new Point(-1, 0);
@@ -8,12 +9,18 @@ describe('Triangle', () => {
   const p3 = new Point(0, 1);
   const triangle = new Triangle(p1, p2, p3);
 
-  it('calculates circumscribed circle correctly', () => {
-    const result = circumscribedCircle([p1, p2, p3])
+  describe('circumCircle', () => {
+    const circumCircle = triangle.circumCircle;
 
-    expect(result.cx).toBe(0);
-    expect(result.cy).toBe(0);
-    expect(result.r).toBe(1);
+    it('is calculated correctly', () => {
+      expect(circumCircle.cx).toBe(0);
+      expect(circumCircle.cy).toBe(0);
+      expect(circumCircle.r).toBe(1);
+    });
+
+    it('is a Circle', () => {
+      expect(circumCircle instanceof Circle).toBeTruthy();
+    })
   });
 
   it('returns a list of Edges', () => {
@@ -25,5 +32,5 @@ describe('Triangle', () => {
 
   it('stores a copy of the circumCircle', () => {
     expect(triangle.circumCircle).toBeDefined();
-  })
+  });
 })
