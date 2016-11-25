552592f39bd0f42cb812209a84d37b78c3a4faf5
James Schwartz <james@schwartz.engineer>
2016-11-25 16:26:41 -0500

    Implementation of SVG rendering.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react';
+import SVG from './SVG'
+import Delaunay from './Delaunay'
+import ControlPanel from './components/ControlPanel'
+import './index.css'
+
+let i = 0;
+const COLORS = ["red", "green", "blue", "yellow"];
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      delaunay: new Delaunay(),
+      style: {
+        point: {
+          r: 5,
+          fill: "magenta"
+        },
+        triangle: {
+          fill: "none",
+          stroke: "magenta",
+          strokeWidth: 3
+        },
+        cell: {
+          stroke: "blue",
+          fill: "grey",
+          fillOpacity: 0.2
+        }
+      }
+    };
+  }
+
+  addPoint(point) {
+    let delaunay = this.state.delaunay;
+    delaunay.addPoint(point);
+
+    this.setState({delaunay})
+  }
+
   render() {
+    const style = this.state.style;
+
+    let geometry = {points: [], cells: [], triangles: []}
+
+    this.state.delaunay.points.forEach((point, index) => {
+      if(!point.cellColor) { point.cellColor = COLORS[++i % 4] }
+
+      geometry.points.push(<circle key={"point_" + index} cx={point.x} cy={point.y} {... style.point } />);
+      geometry.cells.push(<polygon key={"cell_" + index} points={point.voronoiNodes.join(' ')} {... style.cell } fill={point.cellColor} />);
+    });
+
+    this.state.delaunay.triangles.forEach((triangle, index) => {
+      geometry.triangles.push(<polygon key={"triangle_" + index} points={triangle.toString()} {... style.triangle }/>);
+    })
+
     return (
-      <div>Hello World</div>
+      <div>
+        <SVG onClick={this.addPoint.bind(this)}>
+          <g className="cells">{geometry.cells}</g>
+          <g className="points">{geometry.points}</g>
+          <g className="triangles">{geometry.triangles}</g>
+        </SVG>
+      </div>
     );
   }
 }
--- src/SVG.js
+++ src/SVG.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
-const svgNS = "http://www.w3.org/2000/svg";
-
 export default class SVG extends Component {
-  constructor(props) {
-    super(props)
+  componentDidMount() {
+    this.svg = document.querySelector('svg');
+  }
 
+  getClickSVGPoint(e) {
+    let pt = this.svg.createSVGPoint();
+    pt.x = e.clientX;
+    pt.y = e.clientY;
+    pt = pt.matrixTransform(this.svg.getScreenCTM().inverse())
+
+    return { x: pt.x, y: pt.y };
+  }
+
+  onClick(e) {
+    if(this.props.filterClick && this.props.filterClick(e)) return;
+
+    this.props.onClick(this.getClickSVGPoint(e));
   }
 
   render() {
@@ -13,7 +25,15 @@ export default class SVG extends Component {
       <svg
         id="svg"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1000 1000" />
+        viewBox={this.props.viewBox}
+        onClick={this.onClick.bind(this)} >
+        {this.props.children}
+      </svg>
     );
   }
 }
+
+SVG.defaultProps = {
+  viewBox: "0 0 1000 1000",
+  filterClick: () => { return false }
+};
